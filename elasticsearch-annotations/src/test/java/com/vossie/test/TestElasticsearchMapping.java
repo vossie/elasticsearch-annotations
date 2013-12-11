b37442dd3fa7151022f487d4dfa2fbe4c92aa073
vossie <carel.vosloo@bronzegate.com>
2013-12-11 14:35:13 +0000

    Fixed array detection.

--- elasticsearch-annotations/src/test/java/com/vossie/test/TestElasticsearchMapping.java
+++ elasticsearch-annotations/src/test/java/com/vossie/test/TestElasticsearchMapping.java
@@ -7,15 +7,23 @@ import com.vossie.elasticsearch.annotations.exceptions.ClassNotAnnotated;
 import com.vossie.elasticsearch.annotations.exceptions.InvalidAttributeForType;
 import com.vossie.elasticsearch.annotations.exceptions.InvalidParentDocumentSpecified;
 import com.vossie.elasticsearch.annotations.util.ESTypeAttributeConstraints;
+import org.elasticsearch.action.admin.cluster.health.ClusterHealthResponse;
+import org.elasticsearch.action.admin.indices.create.CreateIndexRequestBuilder;
+import org.elasticsearch.action.admin.indices.exists.indices.IndicesExistsRequest;
+import org.elasticsearch.cluster.ClusterState;
+import org.elasticsearch.cluster.metadata.IndexMetaData;
+import org.elasticsearch.cluster.metadata.MappingMetaData;
+import org.elasticsearch.indices.IndexAlreadyExistsException;
+import org.elasticsearch.node.Node;
+import org.elasticsearch.node.NodeBuilder;
 import org.json.JSONException;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 
 import java.io.IOException;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  * Copyright © 2013 GSMA. GSM and the GSM Logo are registered and owned by the GSMA.
@@ -24,6 +32,24 @@ import static org.junit.Assert.assertTrue;
  * Time: 21:00
  */
 public class TestElasticsearchMapping {
+
+    private static Node node;
+
+    @BeforeClass
+    public static void init() {
+
+        node = NodeBuilder.nodeBuilder()
+                .node();
+
+        ClusterHealthResponse clusterHealthResponse = node
+                .client()
+                .admin()
+                .cluster()
+                .prepareHealth()
+                .setWaitForGreenStatus()
+                .execute()
+                .actionGet();
+    }
 
     @Test(expected = ClassNotAnnotated.class)
     public void testForClassNotAnnotatedException() throws InvalidParentDocumentSpecified, ClassNotAnnotated, InvalidAttributeForType {
@@ -85,5 +111,61 @@ public class TestElasticsearchMapping {
     public void testSettingAnInvalidAttributeForType() throws ClassNotAnnotated, InvalidParentDocumentSpecified, InvalidAttributeForType {
 
         ElasticsearchMapping.getMapping(UserWithInvalidAttribute.class);
+    }
+
+    @Test
+    public void testSavingMappingToElasticInstance() throws InvalidAttributeForType, ClassNotAnnotated, InvalidParentDocumentSpecified, IOException, JSONException {
+
+        ElasticsearchDocumentMetadata documentMetadata = ElasticsearchMapping.getMapping(User.class);
+        assertTrue(createIndex(User.class));
+        MappingMetaData json = getMapping(documentMetadata.getIndexName(), documentMetadata.getTypeName());
+    }
+
+    /******************
+     * Helper Methods *
+     ******************/
+
+    public MappingMetaData getMapping(String indexName, String typeName) {
+
+        ClusterState cs = node.client().admin().cluster().prepareState().setFilterIndices(indexName).execute().actionGet().getState();
+        IndexMetaData imd = cs.getMetaData().index(indexName);
+
+        return imd.mapping(typeName);
+    }
+
+    public boolean indexExists(String index) {
+
+        return node.client().admin().indices().exists(new IndicesExistsRequest(index)).actionGet().isExists();
+    }
+
+    public boolean createIndex(Class<?> clazz) throws ClassNotAnnotated, InvalidParentDocumentSpecified, InvalidAttributeForType, IOException {
+
+        ElasticsearchDocumentMetadata documentMetadata = ElasticsearchMapping.getMapping(clazz);
+        createIndex(documentMetadata.getIndexName());
+
+        node.client()
+                .admin ()
+                .indices()
+                .preparePutMapping (documentMetadata.getIndexName())
+                .setType (documentMetadata.getTypeName())
+                .setSource(documentMetadata.toJson())
+                .execute()
+                .actionGet();
+
+        return true;
+    }
+
+    public boolean createIndex(String index) {
+
+        try {
+            if(!indexExists(index)) {
+
+                CreateIndexRequestBuilder createIndexRequestBuilder = node.client().admin().indices().prepareCreate(index);
+                return createIndexRequestBuilder.execute().actionGet().isAcknowledged();
+            }
+        } catch (IndexAlreadyExistsException e) {
+            return true;
+        }
+        return true;
     }
 }
