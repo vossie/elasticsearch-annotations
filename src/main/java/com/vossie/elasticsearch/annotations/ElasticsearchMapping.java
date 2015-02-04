de2d9d751ff987ad5b879301bdc2f781adfa12d1
rahul-patadia <ronly2008@gmail.com>
2015-02-04 16:06:04 +0000

    Elasticsearch mapping updated to traverse all parents of a given class.

--- src/main/java/com/vossie/elasticsearch/annotations/ElasticsearchMapping.java
+++ src/main/java/com/vossie/elasticsearch/annotations/ElasticsearchMapping.java
@@ -9,8 +9,10 @@ import com.vossie.elasticsearch.annotations.enums.FieldType;
 import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -43,7 +45,7 @@ public abstract class ElasticsearchMapping {
      * @return The ElasticsearchDocument details
      *
      */
-    private static ElasticsearchIndex getElasticsearchIndex(Class<?> clazz) {
+    private static ElasticsearchIndex getElasticsearchIndex(final Class<?> clazz) {
 
         ElasticsearchIndex elasticsearchIndex = clazz.getAnnotation(ElasticsearchIndex.class);
 
@@ -55,18 +57,23 @@ public abstract class ElasticsearchMapping {
         return elasticsearchIndex;
     }
 
-    private static Field[] getAllDeclaredFields(Class<?> clazz) {
+    private static Field[] getAllDeclaredFields(final Class<?> clazz) {
 
         // Get all the fields declared in the class
         Field[] fieldsFromClass = clazz.getDeclaredFields();
 
 
         // Also get the super class fields to support polymorphism
-        Field[] fieldsFromSuper = new Field[0];
+        final List<Field> fieldsFromSuperList = new ArrayList<>();
 
-        if(clazz.getSuperclass() != null)
-            fieldsFromSuper = clazz.getSuperclass().getDeclaredFields();
+        Class<?> current = clazz;
+        while(current.getSuperclass()!=null){ // we don't want to process Object.class
+            for(Field f : current.getSuperclass().getDeclaredFields())
+                fieldsFromSuperList.add(f);
+            current = current.getSuperclass();
+        }
 
+        final Field[] fieldsFromSuper = fieldsFromSuperList.toArray(new Field[fieldsFromSuperList.size()]);
 
         // Copy all the fields into a single array.
         Field[] fields = new Field[fieldsFromClass.length + fieldsFromSuper.length];
@@ -85,9 +92,9 @@ public abstract class ElasticsearchMapping {
      * @param clazz The class to scan
      * @return Returns a list containing the field metadata
      */
-    private static Map<String, ElasticsearchNodeMetadata> getElasticsearchFieldsMetadata(Class<?> clazz) {
+    private static Map<String, ElasticsearchNodeMetadata> getElasticsearchFieldsMetadata(final Class<?> clazz) {
 
-        Map<String, ElasticsearchNodeMetadata> elasticsearchFieldMappings = new HashMap<>();
+        final Map<String, ElasticsearchNodeMetadata> elasticsearchFieldMappings = new HashMap<>();
 
         if(clazz == null)
             return elasticsearchFieldMappings;
@@ -149,7 +156,7 @@ public abstract class ElasticsearchMapping {
     }
 
 
-    private static Map<String, ElasticsearchNodeMetadata> getElasticsearchSystemFieldsMetadata(ElasticsearchField[] systemFields) {
+    private static Map<String, ElasticsearchNodeMetadata> getElasticsearchSystemFieldsMetadata(final ElasticsearchField[] systemFields) {
 
         Map<String, ElasticsearchNodeMetadata> elasticsearchFieldMappings = new HashMap<>();
 
@@ -174,7 +181,7 @@ public abstract class ElasticsearchMapping {
      * @param clazz The class to inspect.
      * @return Returns the meta data used to describe this entity.
      */
-    public static ElasticsearchDocumentMetadata get(Class<?> clazz) {
+    public static ElasticsearchDocumentMetadata get(final Class<?> clazz) {
 
         // Check the cache to see if we have already parsed this reference.
         if(mappingCache.containsKey(clazz))
